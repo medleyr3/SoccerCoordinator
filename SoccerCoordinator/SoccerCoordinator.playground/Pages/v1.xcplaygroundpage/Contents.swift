680fa2d2ca4f97691f7d338442d4a54f532c35f7
russimac <russimac@russimacs-imac.local>
2017-02-07 05:22:13 -0500

    changed data structure

---
+++ SoccerCoordinator/SoccerCoordinator.playground/Pages/v1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//Russell Comment
+
+//Create Required Collections
+
+
+var players2 = [
+        "Joe Smith": ["Height": "42", "Soccer Experience": "YES","Guardian Name": "Jim and Jan Smith", "Team": ""],
+        "Jill Tanner": ["Height": "36", "Soccer Experience": "YES", "Guardian Name" : "Clara Tanner", "Team": ""],
+        "Bill Bon": ["Height": "43", "Soccer Experience": "YES", "Guardian Name" : "Sara and Jenny Bon", "Team": ""],
+        "Eva Gordon": ["Height": "45", "Soccer Experience": "NO", "Guardian Name" : "Wendy and Mike Gordon", "Team": ""],
+        "Matt Gill": ["Height": "40", "Soccer Experience": "NO", "Guardian Name" : "Charles and Sylvia Gill", "Team": ""],
+        "Kimmy Stein": ["Height": "41", "Soccer Experience": "NO", "Guardian Name" : "Bill and Hillary Stein", "Team": ""],
+        "Sammy Adams": ["Height": "45", "Soccer Experience": "NO", "Guardian Name" : "Jeff Adams", "Team": ""],
+        "Karl Saygan": ["Height": "42", "Soccer Experience": "YES", "Guardian Name" : "Heather Bledsoe", "Team": ""],
+        "Suzane Greenberg": ["Height": "44", "Soccer Experience": "YES", "Guardian Name" : "Henrietta Dumas", "Team": ""],
+        "Sal Dali": ["Height": "41", "Soccer Experience": "NO", "Guardian Name" : "Gala Dali", "Team": ""],
+        "Joe Kavalier": ["Height": "39", "Soccer Experience": "NO", "Guardian Name" : "Sam and Elaine Kavalier", "Team": ""],
+        "Ben Finkelstein": ["Height": "44", "Soccer Experience": "NO", "Guardian Name" : "Aaron and Jill Finkelstein", "Team": ""],
+        "Diego Soto": ["Height": "41", "Soccer Experience": "YES", "Guardian Name" : "Robin and Sarika Soto", "Team": ""],
+        "Chloe Alaska": ["Height": "47", "Soccer Experience": "NO", "Guardian Name" : "David and Jamie Alaska", "Team": ""],
+        "Arnold Willis": ["Height": "43", "Soccer Experience": "NO", "Guardian Name" : "Claire Willis", "Team": ""],
+        "Phillip Helm": ["Height": "44", "Soccer Experience": "YES", "Guardian Name" : "Thomas Helm and Eva Jones", "Team": ""],
+        "Les Clay": ["Height": "42", "Soccer Experience": "YES", "Guardian Name" : "Wynonna Brown", "Team": ""],
+        "Herschel Krustofski": ["Height": "45", "Soccer Experience": "YES", "Guardian Name" : "Hyman and Rachel Krustofski", "Team": ""]
+]
+
+
+
+
+
+players2["Joe Smith"]?["Soccer Experience"]
